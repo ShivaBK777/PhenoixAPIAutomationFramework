c26b7334fa602b9c32c20db1be762d4d97800113
Shiva <shivayogi951@gmail.com>
2026-02-01 14:49:44 +0530

    Using Input Stream to read csv

--- src/main/java/com/demo/csv/ReadCSV2.java
+++ src/main/java/com/demo/csv/ReadCSV2.java
@@ -3,27 +3,30 @@ package com.demo.csv;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
-import com.opencsv.exceptions.CsvException;
 
 public class ReadCSV2 {
 
 	public static void main(String[] args) {
 		// Java does not have in-built csv file read method.hence open csv library is added
 		
-		File file=new File(System.getProperty("user.dir")+"\\src\\main\\resources\\testData\\LoginCreds.csv");
-		FileReader fr;CSVReader csvreader=null;
+	/*	File file=new File(System.getProperty("user.dir")+"\\src\\main\\resources\\testData\\LoginCreds.csv");FileReader fr;
 		try {
 			fr=new FileReader(file); csvreader=new CSVReader(fr);
 		} catch (FileNotFoundException e) {
 			
 			e.printStackTrace();
-		}
+		}  */
+		InputStream is=Thread.currentThread().getContextClassLoader().getResourceAsStream("testData/LoginCreds.csv");
+	    InputStreamReader isr=new InputStreamReader(is);
+		CSVReader csvreader=new CSVReader(isr);
+		
 		
 		
 		CsvToBean<UserCredsPOJO> csvToBean=new CsvToBeanBuilder(csvreader)
